7aa896ad9f0a4d90dfe9da0dec648338c47181d4
fummeys <53976947+fummeys@users.noreply.github.com>
2022-01-29 13:33:01 +0100

    change of ownership done

--- resources/views/changeofownership.blade.php
+++ resources/views/changeofownership.blade.php
@@ -2,30 +2,71 @@
 @extends('layouts.dashBoardframe')
 
 @section('content')
-<div class="container">
-
-<h1>Enter contact detail of the Applicant</h1>
-<form action="/newchangeownership" method="post" enctype="multipart/form-data">
- @csrf
-
- <div class="form-group">
-     <label for="appno">Application Number</label>
-    <input type="text" class="form-control" id="appno" name="applicationnumber" value="{{rand(100,500).time()}}">
+<div class="row" style="background-color: #E5E5E5">
+  <div class="col p-5">
+    <div class="row pb-3">
+      <div class="col">
+        <h3 style="font-weight: 600; color: #383838">Change of Ownership</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <form action="/newchangeownership" method="post" enctype="multipart/form-data" class="SupportForm p-5">
+            @csrf
+            <div class="container">
+              <h4 style="font-weight: 500;color: #565555">Enter contact detail of the Applicant</h4>
+              <div class="form-group py-3">
+                  <label for="appno label">Application Number:</label>
+                  <input type="text" class="form-control SupportFormInput" id="appno" name="applicationnumber" value="{{rand(100,500).time()}}" readonly>
+                </div>
+              <div class="form-group py-3">
+                  <input type="text" class="form-control SupportFormInput" name="applicantphone" placeholder="Enter phone munber of applicant">
+              </div>
+              <div class="form-group py-3">
+                <input type="text" class="form-control SupportFormInput"  name="applicantaddress" placeholder="Enter email address of applicant">
+              </div>
+              <div class="form-group py-3">
+                <input type="text" class="form-control SupportFormInput"  name="applicantfax" placeholder="Enter fax for applicant">
+              </div>
+              <div class="row py-3">
+                <div class="col-4">
+                  Upload Copy of Transferee Identity Card
+                </div>
+                <div class="col">
+                  <input type="file" name="transfereeid" id="">
+                </div>
+              </div>
+              <div class="row py-3">
+                <div class="col-4">
+                  Upload Copy Tax Clearance certificate
+                </div>
+                <div class="col">
+                  <input type="file" name="clearancecertificate" id="">
+                </div>
+              </div>
+              <div class="row py-3">
+                <div class="col-4">
+                  Upload other supporting documents
+                </div>
+                <div class="col">
+                  <input type="file" name="otherdocuments" id="">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <b style="color: #EE6243;">You are required to make a payment of P500 before your application can be submitted.</b>
+                </div>
+              </div>
+              <div class="row py-3">
+                <div class="col-12 text-end">
+                  <input class="btn btn-primary" type="submit" name="change"/>
+                </div>
+              </div>
+              
+            </div>
+        </form>
+      </div>
+    </div>
   </div>
-<div class="form-group">
-    <input type="text" class="form-control" name="applicantphone" placeholder="Enter phone munber of applicant">
-  </div>
-  <div class="form-group">
-    <input type="text" class="form-control"  name="applicantaddress" placeholder="Enter email address of applicant">
-  </div>
-  <div class="form-group">
-    <input type="text" class="form-control"  name="applicantfax" placeholder="Enter fax for applicant">
-  </div>
-
-  <input type="file" name="transfereeid" id="">
-  <input type="file" name="clearancecertificate" id="">
-  <input type="file" name="otherdocuments" id="">
-  <input type="submit" name="change"/>
-</form>
 </div>
 @endsection
